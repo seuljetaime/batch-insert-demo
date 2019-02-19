8bbefb37a35d7f7e2f8be63ce44fb985e78dfcc4
seuljetaime <seuljetaime@gmail.com>
2019-02-19 17:56:26 +0800

    batch insert

--- src/main/java/com/example/demo/CityMapper.java
+++ src/main/java/com/example/demo/CityMapper.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Mapper
 @Repository
@@ -10,5 +13,10 @@ public interface CityMapper {
 
     @Select("SELECT id, name, state, country FROM city WHERE state = #{state}")
     City findByState(String state);
+
+    @Insert("INSERT into city(id, name, state, country) VALUES(#{id}, #{name}, #{state}, #{country})")
+    void insert(City city);
+
+    void batchInsert(List<City> cityList);
 
 }
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,25 +1,61 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	private final CityMapper cityMapper;
+    private final CityMapper cityMapper;
 
-	public DemoApplication(CityMapper cityMapper) {
-		this.cityMapper = cityMapper;
-	}
+    public DemoApplication(CityMapper cityMapper) {
+        this.cityMapper = cityMapper;
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-	@Bean
-	CommandLineRunner sampleCommandLineRunner() {
-		return (args) -> System.out.println(this.cityMapper.findByState("CA"));
-	}
+    @Bean
+    CommandLineRunner sampleCommandLineRunner() {
+
+        long start = System.currentTimeMillis();
+
+        batchInsert();
+
+        long end = System.currentTimeMillis();
+        return (args) -> System.out.println("插入1w耗时：" + (end - start));
+    }
+
+    // 插入1w耗时：354748
+    void normalInsert() {
+        // 执行循环插入
+        City city = new City();
+        for (int i = 1; i < 10000; i++) {
+            city.setId(Long.valueOf(i));
+            city.setName(i + "");
+            city.setState(i + "");
+            city.setCountry(i + "");
+            cityMapper.insert(city);
+        }
+    }
+
+    void batchInsert() {
+        List<City> cityList = new ArrayList<>();
+
+        for (int i = 1; i < 10000; i++) {
+            City city = new City();
+            city.setId(Long.valueOf(i));
+            city.setName(i + "");
+            city.setState(i + "");
+            city.setCountry(i + "");
+            cityList.add(city);
+        }
+        cityMapper.batchInsert(cityList);
+    }
 }
