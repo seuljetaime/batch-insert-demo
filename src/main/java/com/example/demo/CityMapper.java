ce4b64deac35121ae6c58e7d26aa462374995ac0
seuljetaime <seuljetaime@gmail.com>
2019-02-19 10:56:27 +0800

    完全mybatis官方的demo

--- src/main/java/com/example/demo/CityMapper.java
+++ src/main/java/com/example/demo/CityMapper.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 
 @Mapper
+@Repository
 public interface CityMapper {
 
     @Select("SELECT id, name, state, country FROM city WHERE state = #{state}")
